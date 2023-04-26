b12a3e6216655ff65f5e19820b0f11f17905574c
Pedroart <pedroarteta@hotmail.es>
2023-04-25 19:27:08 -0500

    Desarrollo de Plantilla

--- core/view.php
+++ core/view.php
@@ -1,5 +1,18 @@
 <?php
 
-namespace core;
+function view($route, $data = []){
 
-function view(){}
+    extract($data);
+    if ( file_exists(_view_."/{$route}.php") ){
+        
+        ob_start();
+        include _view_."/{$route}.php";
+        $contenido = ob_get_clean();
+        
+        include _view_."/plantilla.php";
+        
+    }
+    else {
+        echo  "noexiste";
+    }
+}
