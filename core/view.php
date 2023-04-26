7a733fb121f3abcab6c2482cadbd43d7c0f00bb5
Pedroart <pedroarteta@hotmail.es>
2023-04-25 22:55:13 -0500

    Desarrollo

--- core/view.php
+++ core/view.php
@@ -5,6 +5,19 @@ function view($route, $data = []){
     extract($data);
     if ( file_exists(_view_."/{$route}.php") ){
         
+        include _view_."/{$route}.php";
+                
+    }
+    else {
+        echo  "noexiste";
+    }
+}
+
+function view_dashboard($route, $data = []){
+
+    extract($data);
+    if ( file_exists(_view_."/{$route}.php") ){
+        
         ob_start();
         include _view_."/{$route}.php";
         $contenido = ob_get_clean();
