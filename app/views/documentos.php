31fb6f08a75f9e391ec2456fae9bb4828951b6a5
Pedroart <pedroarteta@hotmail.es>
2023-10-14 20:08:57 -0500

    ansosos

--- app/views/documentos.php
+++ app/views/documentos.php
@@ -1,15 +1,30 @@
 <div class="container px-4 py-5" id="featured-3">
     <h2 class="pb-2 border-bottom">Proceso de practicas Efectivas</h2>
     <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+    <?php if ($_SESSION['role'] == "3") :?>  
       <div class="feature col">
-        
-        <h2>Featured title</h2>
-        <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-        <a href="#" class="icon-link">
-          Descargar
-          
-        </a>
+        <h2>Anexo 4</h2>
+        <p>ESTRUCTURA DEL PLAN DE TRABAJO DE PPP.</p>
+        <a href="<?= _URL_ ?>/Formatos/Documento Alumno/Anexo 4 - ESTRUCTURA DEL PLAN DE TRABAJO DE PPP.pdf" download="Anexo 4 - ESTRUCTURA DEL PLAN DE TRABAJO DE PPP.pdf" class="icon-link">Descargar</a>
       </div>
-      
+      <div class="feature col">
+        <h2>Anexo 5</h2>
+        <p>ESTRUCTURA DEL INFORME FINAL DE PPP.</p>
+        <a href="#" class="icon-link">Descargar</a>
+      </div>
+      <div class="feature col">
+        <h2>CARATULA DE FILE PPP</h2>
+        <a href="#" class="icon-link">Descargar</a>
+      </div>
+      <div class="feature col">
+        <h2>FORMATOS ANEXO 3</h2>
+        <a href="#" class="icon-link">Descargar</a>
+      </div>
+      <div class="feature col">
+        <h2>FORMATOS ANEXO 6</h2>
+        <a href="#" class="icon-link">Descargar</a>
+      </div>
+    <?php else:?>
+    <?php endif; ?>
     </div>
   </div>
